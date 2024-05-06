bf4194af139d8b0ee7c99789779bfe5fd434a124
leif160519 <yzluofei@126.com>
2024-05-06 10:13:12 +0800

    +velero update install.sh change minio console ip -> domain

--- velero/install.sh
+++ velero/install.sh
@@ -6,6 +6,6 @@ REGION=minio
     --provider aws \
     --plugins velero/velero-plugin-for-aws:v1.4.0 \
     --bucket ${BUCKET} \
-    --backup-location-config region=$REGION,s3ForcePathStyle="true",s3Url=http://minio.velero.svc:9000 \
-    --snapshot-location-config region=$REGION,s3ForcePathStyle="true",s3Url=http://minio.velero.svc:9000 \
+    --backup-location-config region=$REGION,s3ForcePathStyle="true",s3Url=https://console.github.icu \
+    --snapshot-location-config region=$REGION,s3ForcePathStyle="true",s3Url=https://console.github.icu \
     --secret-file /opt/velero/cfg/credentials-velero
